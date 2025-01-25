9648d63c16359953da486f1ee0e3a68c69218b5f
Eileen <93325000+Eileen-10@users.noreply.github.com>
2025-01-26 03:37:22 +0800

    Modified to print mapper exec time too

--- CST435-Reducer.py
+++ CST435-Reducer.py
@@ -5,20 +5,29 @@ import time
 def reducer():
     start_time = time.time()  # Start timing
     numbers = []
+    mapper_times = []
+
     for line in sys.stdin:
         if line.startswith("Unsorted Data:"):
             print(line.strip())
             continue
         try:
             key, value = line.strip().split('\t')
-            numbers.append(int(value))
+            if key == "MapperExecutionTime":
+                mapper_times.append(float(value))
+            else:
+                 numbers.append(int(value))
         except ValueError:
             continue
     
     numbers.sort()
     print(f'Sorted Data: {numbers}')
+
+    total_mapper_time = sum(mapper_times)
+    print(f"Mapper Execution Time: {total_mapper_time:.6f} seconds")
+
     end_time = time.time()  # End timing
-    print(f"Reducer Execution Time: {end_time - start_time:.6f} seconds", file=sys.stderr)
+    print(f"Reducer Execution Time: {end_time - start_time:.6f} seconds")
 
 if __name__ == '__main__':
     reducer()
